d7afeb67d904a8634aa17ab42c484d59f2e20fdc
Erameh Michael <eramehmichael2000@gmail.com>
2023-03-18 09:49:08 -0700

    me

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -2,4 +2,4 @@ SELECT `score`, `name`
 FROM `second_table`
 WHERE `score` >= 10
 ORDER BY `score` DESC;
---Write a script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Write a script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
