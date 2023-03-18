a62e8024c667677490116e7217ea1f6346b6a1be
Erameh Michael <eramehmichael2000@gmail.com>
2023-03-18 10:03:56 -0700

    me

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -2,4 +2,4 @@ SELECT `score`, `name`
 FROM `second_table`
 WHERE `name` != ""
 ORDER BY `score` DESC;
---Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
