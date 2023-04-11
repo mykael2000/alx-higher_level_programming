f870afcd992d1ae1f9c51fef1a795ae50eda14e1
Erameh Michael <eramehmichael2000@gmail.com>
2023-04-11 13:50:30 -0700

    me

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let num = process.argv[2];
+
+if (num === undefined || isNaN(num)){
+	console.log("Missing size");
+} else {
+	const re = Number(num);
+	let i = 0;
+	while (i < x) {
+		console.log("X".repeat(x));
+		i++;
+	}
+}
+
