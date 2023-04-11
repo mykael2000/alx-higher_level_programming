fa3dd6e73efa89ada28ba8cb3c3d187092493410
Erameh Michael <eramehmichael2000@gmail.com>
2023-04-11 12:45:38 -0700

    me

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 let num1 = Number.isInteger(process.argv[2]);
 let num2 = Number.isInteger(process.argv[2]);
 
-if(num1 || num2 === false) {
+if(num1 || num2 === "false") {
 	console.log("Not a number");
 } else {
 	console.log("My number:", parseInt(process.argv[2]));
