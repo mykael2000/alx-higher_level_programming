a14ddc5b6c740fb32a62d285e242fdfa95c32122
Erameh Michael <eramehmichael2000@gmail.com>
2023-04-11 11:02:45 -0700

    me

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-if(valid.argv.length === 0){
+if(valid.argv.length === 2){
 	console.log("No argument");
-}elseif(valid.argv.length === 1){
+}elseif(valid.argv.length === 3){
 	console.log("Argument found");
 }else{
 	console.log("Arguments found");
