7ff8e96189b4f93a3fc7c4748c8a1c41fd10fd30
Erameh Michael <eramehmichael2000@gmail.com>
2023-04-17 15:06:25 -0700

    me

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+""" This is used to list states in database hbtn_0e_0_usa """
+import MySQLdb
+import sys
 
-import MySQLdb;
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="mYk@2004!", db="my_db", charset="utf8");
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    item = db.curson()
+    item.execute("SELECT * FROM states")
+    values = item.fetchall()
+    for value in values:
+        print(value)
+    item.close()
+    db.close()
