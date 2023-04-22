191e552e1ba8920ea48a1a123116acf239aaf7bf
Erameh Michael <eramehmichael2000@gmail.com>
2023-04-21 20:02:41 -0700

    me

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Displays all values in the states table of hbtn_0e_0_usa
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    item = conn.cursor()
+    item.execute("SELECT * FROM states WHERE name LIKE BINARY `sys.argv[4]`")
+    getter = item.fetchall()
+    for get in getter:
+        print(get)
+
+    item.close() 
+    conn.close()
+
