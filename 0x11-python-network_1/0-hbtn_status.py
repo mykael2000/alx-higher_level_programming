c2c5e85329ef98363481039237a7324c63f8d6b9
Erameh Michael <eramehmichael2000@gmail.com>
2023-04-28 14:14:59 -0700

    me

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status """
+
+import urllib.request
+
+if __name__ == '__main__':
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as reply:
+        message = reply.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(message)))
+        print("\t- content: {}".format(message))
+        print("\t- utf8 content: {}".format(message.decode('utf-8')))
+
