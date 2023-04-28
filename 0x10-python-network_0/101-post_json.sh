86b8e39625508e8b2c53976bdc9b3fdef3fbcd50
Erameh Michael <eramehmichael2000@gmail.com>
2023-04-27 20:13:51 -0700

    me

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a JSON POST request to a URL passed as the first argument
-curl -sXH "$1" -d "@$2" POST  "Content-Type: application/json"
+curl -sXH -d "$(cat"$2")" "$1" POST  "Content-Type: application/json"
