87f60a78ab93f839bf6169293c1bf646a38e7683
Erameh Michael <eramehmichael2000@gmail.com>
2023-04-27 19:27:49 -0700

    me

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # bash script that takes in a URL and displays allow options
-curl -s -I -X | grep "Allow:" | cut -d " " -f 2-
+curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
