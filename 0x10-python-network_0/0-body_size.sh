96fd7c4c87862b7fec7d0798b24ff2cc9203b251
Erameh Michael <eramehmichael2000@gmail.com>
2023-04-27 19:16:18 -0700

    me

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a request to that URL
-curl -Is "$1" | grep "Content-Length:" | cut -d " " -f 2
+curl -I -s "$1" | grep "Content-Length:" | cut -d " " -f 2
